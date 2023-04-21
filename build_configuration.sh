c967af13762e48ca265313ce695dd2946ecd0f24
Michael O’Brien <mobrien@cheekyghost.com>
2023-04-21 21:01:12 +1000

    Updating .gitignore

--- build_configuration.sh
+++ build_configuration.sh
@@ -4,9 +4,6 @@ EXCLUDE_SYSTEM="true"
 EXCLUDE_STALE="true"
 CONFIG_JSON="{\"projectDirectory\": \"$PROJECT_DIR\", \"excludeSystemResults\": $EXCLUDE_SYSTEM, \"excludeStaleResults\": $EXCLUDE_STALE}"
 CONFIG_PATH="${PROJECT_DIR}/Tests/IndexStoreTests/Configurations/test_configuration.json"
-if [ -f "$CONFIG_PATH" ] ; then
-    rm -rf "$CONFIG_PATH"
-fi
-touch "$CONFIG_PATH"
+echo "" > "$CONFIG_PATH"
 echo "$CONFIG_JSON" > "$CONFIG_PATH"
-echo "test_configuration.json created at '$CONFIG_PATH'"
+echo "test_configuration.json populated at '$CONFIG_PATH'"
