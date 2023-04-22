f454aad0c68c4d6a4f09067c62bae90a1fb2e607
Michael O’Brien <mobrien@cheekyghost.com>
2023-04-22 23:56:45 +1000

    Adding test placeholders

--- Tests/IndexStoreTests/IndexStore/IndexStoreTests.swift
+++ Tests/IndexStoreTests/IndexStore/IndexStoreTests.swift
@@ -9,38 +9,7 @@ import XCTest
 import Files
 @testable import IndexStore
 
-final class IndexStoreTests: XCTestCase {
-
-    // MARK: - Properties
-    var sourceCreated: Bool = false
-    var instanceUnderTest: IndexStore!
-
-    // MARK: - Lifecycle
-
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        let configuration = try loadDefaultConfiguration()
-        instanceUnderTest = IndexStore(configuration: configuration, logger: .test)
-    }
-
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        instanceUnderTest = nil
-    }
-
-    // MARK: - Helpers
-
-    func loadDefaultConfiguration() throws -> Configuration {
-        let configPath = "\(Bundle.module.resourcePath ?? "")/Configurations/test_configuration.json"
-        let configUrl = URL(fileURLWithPath: configPath)
-        let data = try Data(contentsOf: configUrl)
-        let decoded = try JSONDecoder().decode(Configuration.self, from: data)
-        return decoded
-    }
-
-    func pathSuffix(_ sourceName: String) -> String {
-        "IndexStore/Tests/IndexStoreTests/Samples/\(sourceName)"
-    }
+final class IndexStoreTests: IndexStoreTestCase {
 
     // MARK: - Tests: Direct
 
