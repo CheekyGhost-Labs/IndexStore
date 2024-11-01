e3f60522ce7720b962d59a168ecf981fd87122e9
Michael O’Brien <mobrien@cheekyghost.com>
2024-11-01 23:40:21 +1000

    Re-adding runners

--- Sources/IndexStore/Internal/Extensions/IndexStoreDB+Hashable.swift
+++ Sources/IndexStore/Internal/Extensions/IndexStoreDB+Hashable.swift
@@ -9,6 +9,7 @@ import IndexStoreDB
 
 // Extensions that adds Hashable support to IndexStoreDB types so can use OrderedSet results
 
+#if swift(>=6.0)
 extension SymbolOccurrence: @retroactive Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(symbol.hashValue)
@@ -17,7 +18,18 @@ extension SymbolOccurrence: @retroactive Hashable {
         hasher.combine(relations.hashValue)
     }
 }
+#else
+extension SymbolOccurrence: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(symbol.hashValue)
+        hasher.combine(location.hashValue)
+        hasher.combine(roles.hashValue)
+        hasher.combine(relations.hashValue)
+    }
+}
+#endif
 
+#if swift(>=6.0)
 extension SymbolLocation: @retroactive Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(path.hashValue)
@@ -27,10 +39,30 @@ extension SymbolLocation: @retroactive Hashable {
         hasher.combine(utf8Column.hashValue)
     }
 }
+#else
+extension SymbolLocation: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(path.hashValue)
+        hasher.combine(moduleName.hashValue)
+        hasher.combine(isSystem.hashValue)
+        hasher.combine(line.hashValue)
+        hasher.combine(utf8Column.hashValue)
+    }
+}
+#endif
 
+#if swift(>=6.0)
 extension SymbolRelation: @retroactive Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(symbol.hashValue)
         hasher.combine(roles.hashValue)
     }
 }
+#else
+extension SymbolRelation: @retroactive Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(symbol.hashValue)
+        hasher.combine(roles.hashValue)
+    }
+}
+#endif
